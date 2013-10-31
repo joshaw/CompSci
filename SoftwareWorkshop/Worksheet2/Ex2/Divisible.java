8132c046fae91244ae4d793ad8984b4bc17ac856
Josh Wainwright <wainwright.ja@gmail.com>
2013-10-31 10:37:30 +0000

    Moved to class structure with test class. Implemented get methods and toString

--- SoftwareWorkshop/Worksheet2/Ex2/Divisible.java
+++ SoftwareWorkshop/Worksheet2/Ex2/Divisible.java
@@ -1,4 +1,4 @@
-/** Defines a program to prints, without duplicates, the integers between 0 and
+/** Defines a program to print, without duplicates, the integers between 0 and
  * "maximum" which are divisible by any of 2, 3, and 5, but not by any of 7 and
  * 11 in increasing order.
  *
@@ -6,43 +6,87 @@
  * UID       : 1079596
  * Worksheet : 2
  * Exercise  : 2
+ * File name : Divisible.java
  * @version 2013-11-07
  */
 public class Divisible {
 
-	public static void main(String[] args) {
+	private int maximum = 300;
+	private String numbers = "";
+	private int count = 0;
 
-		int maximum = 300;
+	/** Constructor for the Divisible class
+	 *
+	 * @param maximum starting from 0 to "maximum", find the integers that
+	 * satisfy the conditions.
+	 */
+	public Divisible(int maximum){
+		this.maximum = maximum;
+	}
+
+	/** Method to find all the integers, without duplicates, from 0 to
+	 * "maximum" that satisfy the conditions specified. Stores the resulting
+	 * formatted string in the variable called "numbers".
+	 */
+	public void findDivisible(){
+
 		int columns = 0;
-
-		// Accept argument to program as "maximum" if it exists.
-		if (args.length == 1) {
-			int temp = Integer.parseInt(args[0]);
-			if (temp > 2) {
-				maximum = temp;
-			}
-		}
 
 		// Calculate the nessessary width of each column based on the number of
 		// digits needed for maximum, the largest value to be printed.
 		int width = (int) Math.log10(maximum) + 2;
 
-		//Main loop. For each integer from 0 to maximum, check if conditions
-		//apply. If true, then print number with appropriate spacing.
+		// Main loop. For each integer from 0 to maximum, check if conditions
+		// apply. If true, then print number with appropriate spacing.
 		for (int i = 0; i < maximum; i++) {
-			if (((i%2==0) || (i%3==0) || (i%5==0)) && ((i%7!=0) || (i%11!=0))) {
-				System.out.printf("%"+width+"s", i);
+
+			// (2 OR 3 OR 5) AND (NOT 7 AND NOT 11)
+			if (((i%2==0) || (i%3==0) || (i%5==0)) && ((i%7!=0) && (i%11!=0))) {
+				numbers += String.format("%"+width+"s", i);
 				columns++;
+				count++;
 			}
 			// Print a newline when 20 columns have been printed, unless the
 			// last number is the last in the row.
 			if ( ( columns==20 ) && ( i<maximum-1 ) ) {
-				System.out.println();
+				numbers += "\n";
 				columns = 0;
 			}
 		}
-		// Finish last entry with newline.
-		System.out.println();
+	}
+
+	/** Method for returning the string that contains the results of the
+	 * findDivisible method.
+	 *
+	 * @return the contents of the string numbers. If the findDivisible method
+	 * has been called, then this contains the integers that were found,
+	 * otherwise is the null string.
+	 */
+	public String getNumbers() {
+		return numbers;
+	}
+
+	/** Method for returning the number of integers found when calling the
+	 * {@link #findDivisible} method.
+	 *
+	 * @return the number of integers found
+	 */
+	public int getCount() {
+		return count;
+	}
+
+	/** Defines how to print a Divisible class object
+	 *
+	 * @return the string containing the search results
+	 */
+	@Override
+	public String toString() {
+		if (count == 0) {
+			return "Either there were no numbers found, or the " +
+				"\"findDivisible\" method has not been run.";
+		} else {
+			return numbers;
+		}
 	}
 
 }
---
+++ SoftwareWorkshop/Worksheet2/Ex2/DivisibleTest.java
@@ -0,0 +1,45 @@
+/** Test class for the Divisible class which finds integers that match certain
+ * parameters.
+ *
+ * @author Josh Wainwright
+ * UID       : 1079596
+ * Worksheet : 2
+ * Exercise  : 2
+ * File name : DivisibleTest.java
+ * @version 2013-11-07
+ */
+public class DivisibleTest {
+	public static void main(String[] args) {
+
+		int maximum = 300;
+
+		try{
+			// Accept argument to program as "maximum" if it exists.
+			if (args.length > 0) {
+				int temp = Integer.parseInt(args[0]);
+				if (temp > 2) {
+					maximum = temp;
+				}
+			}
+		}catch(NumberFormatException e){
+			System.out.println("Not a valid input. Using default value 300");
+			System.out.println();
+		}
+
+		Divisible d = new Divisible(maximum);
+
+		d.findDivisible();
+
+		System.out.println();
+
+		System.out.println(d.getNumbers());
+		System.out.println("\nThere are " + d.getCount() + " numbers that satisfy");
+
+		System.out.println();
+
+		Divisible d2 = new Divisible(135);
+		d2.findDivisible();
+		System.out.println(d2.getNumbers());
+		System.out.println("\nThere are " + d2.getCount() + " numbers that satisfy");
+	}
+}
