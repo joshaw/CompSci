474b846849f01ff1e9825a79958d2ed272ab4aaf
Josh Wainwright (uni) <jaw097@bham.ac.uk>
2013-11-04 15:23:10 +0000

    Started move to JUnit

--- SoftwareWorkshop/Worksheet2/Ex1/EqualsTest.java
+++ SoftwareWorkshop/Worksheet2/Ex1/EqualsTest.java
@@ -7,27 +7,35 @@
  * File name : EqualsTest.java
  * @version 2013-11-07
  */
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class EqualsTest {
 	/** Test function for the Equals method
 	 */
-	public static void main(String[] args){
+	// public static void main(String[] args){
 
-		// Use two arguements to the program.
-		if (args.length == 2) {
-			if(Equals.equals(args[0], args[1])){
-				System.out.println("Strings are Equal");
-			}else{
-				System.out.println("Strings are Different");
-			}
-		}
+	// 	// Use two arguements to the program.
+	// 	if (args.length == 2) {
+	// 		if(Equals.equals(args[0], args[1])){
+	// 			System.out.println("Strings are Equal");
+	// 		}else{
+	// 			System.out.println("Strings are Different");
+	// 		}
+	// 	}
 
-		System.out.println(Equals.equals("test","tesT"));     // true
-		System.out.println(Equals.equals("test","TEST"));     // true
-		System.out.println(Equals.equals("test","TesT"));     // true
-		System.out.println(Equals.equals("test","tESt"));     // true
-		System.out.println(Equals.equals("test","tes"));      // false
-		System.out.println(Equals.equals("test","testS"));    // false
-		System.out.println(Equals.equals("test",""));         // false
+	// 	System.out.println(Equals.equals("test","tesT"));     // true
+	// 	System.out.println(Equals.equals("test","TEST"));     // true
+	// 	System.out.println(Equals.equals("test","TesT"));     // true
+	// 	System.out.println(Equals.equals("test","tESt"));     // true
+	// 	System.out.println(Equals.equals("test","tes"));      // false
+	// 	System.out.println(Equals.equals("test","testS"));    // false
+	// 	System.out.println(Equals.equals("test",""));         // false
+	// }
+
+	@Test
+	public void TestEquals(){
+		assertTrue("The strings should be equal", Equals.equals("test", "tesT"));
 	}
 
 }
