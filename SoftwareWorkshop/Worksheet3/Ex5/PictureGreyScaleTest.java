78e7045a245124aa8b4e54f21310a51824578123
Josh Wainwright <wainwright.ja@gmail.com>
2013-11-14 22:37:29 +0000

    Test files moved to root folder

--- SoftwareWorkshop/Worksheet3/Ex5/PictureGreyScaleTest.java
+++ SoftwareWorkshop/Worksheet3/Ex5/PictureGreyScaleTest.java
@@ -1,13 +1,70 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class PictureGreyScaleTest {
 	public static void main(String[] args) {
-		PictureGreyScale compsci = new PictureGreyScale("ComputerScience", "brightness", true);
+		try {
+			System.out.println("Reading File...");
+			PictureGreyScale compsci = new
+				PictureGreyScale("../ComputerScience", "brightness", true);
 
-		System.out.println(compsci.getFilename());
-		System.out.println(compsci.getfileType());
-		System.out.println(compsci.getX());
-		System.out.println(compsci.getY());
+			String[] methods = {"brightness", "luminosity", "average"};
+			for (String method: methods) {
+				compsci.setMethod(method);
 
-		compsci.greyScalePicture(true);
+				System.out.println("Filename       : " + compsci.getFilename());
+				System.out.println("Filetype       : " + compsci.getFiletype());
+				System.out.println("X dimension    : " + compsci.getX());
+				System.out.println("Y dimension    : " + compsci.getY());
+				System.out.println("Convert method : " + compsci.getMethod());
 
+				System.out.println();
+				System.out.println("Converting and Writing File...");
+				compsci.greyScalePicture(true);
+			}
+		} catch (IOException e){
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+	@Test
+	public void PictureGreyScaleTest1() {
+
+		try{
+			PictureGreyScale t1 = new PictureGreyScale("../test", "brightness", false);
+			t1.greyScalePicture(false);
+
+			String testImageString = "P2" + "\n" +
+			                         "3 2" + "\n" +
+			                         "255" + "\n" +
+			                         "127 127 127 " + "\n" +
+			                         "127 255   0 " + "\n";
+			assertEquals(testImageString, t1.getNewImage());
+
+		} catch (IOException e){
+			System.err.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public void PictureGreyScaleTest2() {
+
+		try{
+			PictureGreyScale t2 = new PictureGreyScale("../test", "luminosity", false);
+			t2.greyScalePicture(false);
+
+			String testImageString = "P2" + "\n" +
+			                         "3 2" + "\n" +
+			                         "255" + "\n" +
+			                         " 53 181  17 " + "\n" +
+			                         "234 252   0 " + "\n";
+			assertEquals(testImageString, t2.getNewImage());
+
+		} catch (IOException e){
+			System.err.println(e.getMessage());
+		}
 	}
 }
