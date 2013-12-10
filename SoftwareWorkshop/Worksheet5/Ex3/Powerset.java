ff492bddefcb2215c667c611bc7753866fc33ac5
Josh Wainwright (uni) <jaw097@bham.ac.uk>
2013-12-10 13:47:13 +0000

    Removed unnessessary attempts

--- SoftwareWorkshop/Worksheet5/Ex3/Powerset.java
+++ SoftwareWorkshop/Worksheet5/Ex3/Powerset.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 
-public class Powerset {
+public class Powerset extends Set {
 
 	public static void main(String[] args) {
 		Set s =
 			Set.cons(1, Set.cons(2, Set.cons(3, Set.cons(4, Set.empty()))));
 		System.out.println(s);
-		Powerset.powerset(s);
-		System.out.println(power);
+		System.out.println(Powerset.powerSet(s));
+
+		Set r =
+			Set.cons(1, Set.cons(2, Set.cons(3, Set.cons(4, Set.empty()))));
+		System.out.println(r);
+
 	}
 
 	// public static ArrayList<Set> powerset2(Set s, ArrayList<Set> power){
@@ -20,53 +24,32 @@ public class Powerset {
 	// 	}
 	// }
 
-	public static ArrayList<Set> power = new ArrayList<Set>();
-	public static Set powerset(Set s){
-		// powerset(string) {
-		// 	add string to set;
-		// 	for each char in string {
-		// 		let substring = string excluding char,
-		// 		add powerset(substring) to set
-		// 	}
-		// 	return set;
-		// }
-		System.out.println(power);
-		power.add(s);
-		if (s.isEmpty() || s.getTail().isEmpty()) {
-			return new Set();
+	// public static ArrayList<Set> power = new ArrayList<Set>();
+	// public static Set powerset(Set s){
+	// 	System.out.println(power);
+	// 	power.add(s);
+	// 	if (s.isEmpty() || s.getTail().isEmpty()) {
+	// 		return new Set();
+	// 	} else {
+	// 		power.add(powerset(Set.toSet(s.getTail())));
+	// 		return Set.toSet(s.getTail());
+	// 	}
+	// }
+
+	public static ArrayList<Set> powerSet(Set originalSet, ArrayList<Set> sets){
+		if (originalSet.isEmpty()) {
+			sets.add(new Set());
+			return sets;
+		} else if (originalSet.getTail().isEmpty()){
+			sets.add(new Set(originalSet.getHead(), Set.empty()));
+			return sets;
 		} else {
-			power.add(powerset(Set.toSet(s.getTail())));
-			return Set.toSet(s.getTail());
+			sets.add(new Set(originalSet.getHead(), Set.empty()));
+			return powerSet(Set.toSet(originalSet.getTail()), sets);
 		}
 	}
 
 	public static ArrayList<Set> powerSet(Set originalSet){
-		ArrayList<Set> sets = new ArrayList<Set>();
-		if (originalSet.isEmpty()) {
-			sets.add(new Set());
-			return sets;
-		}
+		return powerSet(originalSet, new ArrayList<Set>());
 	}
-	// public static <T> Set<Set<T>> powerSet(Set<T> originalSet) {
-    	// Set<Set<T>> sets = new HashSet<Set<T>>();
-    	// if (originalSet.isEmpty()) {
-    		// sets.add(new HashSet<T>());
-    		// return sets;
-    	// }
-    	// List<T> list = new ArrayList<T>(originalSet);
-    	// T head = list.get(0);
-    	// Set<T> rest = new HashSet<T>(list.subList(1, list.size()));
-    	// for (Set<T> set : powerSet(rest)) {
-    		// Set<T> newSet = new HashSet<T>();
-    		// newSet.add(head);
-    		// newSet.addAll(set);
-    		// sets.add(newSet);
-    		// sets.add(set);
-    	// }
-    	// return sets;
-	// }
-
-	// public static ArrayList<Set> powerset(Set s) {
-	// 	return powerset(s, new ArrayList<Set>());
-	// }
 }
