2344904203743a3c9c9293d5708598d322c23e54
Josh Wainwright <wainwright.ja@gmail.com>
2013-12-11 11:43:57 +0000

    Added some more items and corrected diagrams

--- Databases/AssessedExercise1/astro.sql
+++ Databases/AssessedExercise1/astro.sql
@@ -10,53 +10,72 @@ DROP TABLE Star CASCADE;
 DROP TABLE Elements CASCADE;
 DROP TABLE Star_Elements CASCADE;
 
-CREATE TABLE Object (object_id SERIAL PRIMARY KEY,
-	                 mass      INT CHECK(mass > 0),
-                     avg_dist  INT CHECK(avg_dist > 0)
-                    );
+CREATE TABLE Object (
+	object_id SERIAL PRIMARY KEY,
+	mass      INT    CHECK(mass > 0),
+	avg_dist  INT    CHECK(avg_dist > 0)
+);
 
-CREATE TABLE Planet (object_id   SERIAL REFERENCES Object,
-                     planet_name VARCHAR(30) UNIQUE PRIMARY KEY,
-                     year_length INT CHECK(year_length > 0),
-                     class       CHAR(8)
-                    );
+CREATE TABLE Planet (
+	object_id   SERIAL      REFERENCES Object,
+	planet_name VARCHAR(30) UNIQUE PRIMARY KEY,
+	year_length INT         CHECK(year_length > 0),
+	class       CHAR(8)
+);
 
-CREATE TABLE Component (planet_name  VARCHAR(30) REFERENCES Planet,
-                        component_id SERIAL,
-                        comp_name    VARCHAR(30),
-                        PRIMARY KEY (planet_name, component_id)
-                       );
+CREATE TABLE Component (
+	planet_name  VARCHAR(30) REFERENCES Planet,
+	component_id SERIAL,
+	comp_name    VARCHAR(30),
+	PRIMARY KEY (planet_name, component_id)
+);
 
-CREATE TABLE Asteroid (object_id    SERIAL REFERENCES Object,
-                       asteroid_id  SERIAL PRIMARY KEY,
-                       eccentricity DECIMAL CHECK(eccentricity > 0)
-                      );
+CREATE TABLE Asteroid (
+	object_id    SERIAL REFERENCES Object,
+	asteroid_id  SERIAL PRIMARY KEY,
+	eccentricity DECIMAL CHECK(eccentricity > 0)
+);
 
-CREATE TABLE Discoverer (firstname    VARCHAR(20),
-                         surname      VARCHAR(20),
-                         title        VARCHAR(5) NOT NULL,
-                         organisation VARCHAR(30),
-                         PRIMARY KEY (firstname, surname)
-                        );
+CREATE TABLE Discoverer (
+	firstname    VARCHAR(20),
+	surname      VARCHAR(20) NOT NULL,
+	title        VARCHAR(5)  NOT NULL,
+	organisation VARCHAR(30),
+	PRIMARY KEY (firstname, surname)
+);
 
-CREATE TABLE Disc_by (asteroid_id SERIAL REFERENCES Asteroid,
-                      firstname   VARCHAR(20) REFERENCES Discoverer,
-                      surname     VARCHAR(20) REFERENCES Discoverer,
-                      date_disc   DATE,
-                      PRIMARY KEY (asteroid_id, firstname, surname)
-                     );
+CREATE TABLE Disc_by (
+	asteroid_id SERIAL      REFERENCES Asteroid,
+	firstname   VARCHAR(20) REFERENCES Discoverer,
+	surname     VARCHAR(20) REFERENCES Discoverer,
+	date_disc   DATE,
+	PRIMARY KEY (asteroid_id, firstname, surname)
+);
 
-CREATE TABLE Star (object_id  INT REFERENCES Object,
-                   star_id    SERIAL PRIMARY KEY,
-                   luminosity INT CHECK(luminosity > 0)
-                  );
+CREATE TABLE Star (
+	object_id  INT    REFERENCES Object,
+	star_id    SERIAL PRIMARY KEY,
+	luminosity INT    CHECK(luminosity > 0)
+);
 
-CREATE TABLE Elements (atomic_number INT CHECK(atomic_number > 0) PRIMARY KEY,
-                       atomic_mass   NUMERIC CHECK(atomic_mass > 0)
-                      );
+CREATE TABLE Elements (
+	atomic_number INT     CHECK(atomic_number > 0),
+	atomic_mass   NUMERIC CHECK(atomic_mass > 0),
+	PRIMARY KEY (atomic_number)
+);
 
-CREATE TABLE Star_Elements (star_id INT REFERENCES Star PRIMARY KEY,
-                            atomic_number INT REFERENCES Elements
-                           );
+CREATE TABLE Star_Elements (
+	star_id       SERIAL REFERENCES Star PRIMARY KEY,
+	atomic_number INT    REFERENCES Elements
+);
 
-\dt
+CREATE TABLE orbit_member (
+	star_id   SERIAL REFERENCES Star,
+	orbit_num SERIAL,
+	PRIMARY KEY (star_id, orbit_num)
+);
+
+CREATE TABLE orbit (
+	orbit_num SERIAL  REFERENCES orbit_member PRIMARY KEY,
+	radius    NUMERIC
+);
--- Databases/AssessedExercise1/astroInsert.sql
+++ Databases/AssessedExercise1/astroInsert.sql
@@ -24,6 +24,45 @@ INSERT INTO star VALUES
 	(4, DEFAULT, 7)
 	;
 
+INSERT INTO Star_Elements VALUES
+	(1, 1),
+	(1, 2),
+	(1, 3),
+	(1, 4),
+	(1, 7),
+	(1, 8),
+	(1, 16),
+	(2, 1),
+	(2, 2),
+	(2, 3),
+	(3, 1),
+	(3, 2),
+	(3, 3),
+	(3, 5),
+	(3, 11),
+	(3, 6)
+	;
+
+INSERT INTO Elements VALUES
+	(1, 1.001, 'Hydrogen'),
+	(2, 4.003, 'Helium'),
+	(3, 6.94, 'Lithium'),
+	(4, 9.01, 'Beryllium'),
+	(5, 10.81, 'Boron'),
+	(6, 12.01, 'Carbon'),
+	(7, 14.01, 'Nitrogen'),
+	(8, 15.999, 'Oxygen'),
+	(9, 18.998, 'Flourine'),
+	(10, 20.18, 'Neon'),
+	(11, 22.99, 'Sodium'),
+	(12, 24.31, 'Magnesium'),
+	(13, 26.98, 'Aluminium'),
+	(14, 28.19, 'Silicon'),
+	(15, 30.97, 'Phosphorus'),
+	(16, 32.06, 'Sulphur'),
+	(17, 35.45, 'Chlorine')
+	;
+
 INSERT INTO planet VALUES
 	(5, 'Zark', 1.3, 'rocky'),
 	(6, 'Jupiter', 12, 'rocky'),
@@ -47,4 +86,24 @@ INSERT INTO asteroid VALUES
 	(8, DEFAULT, 0.8),
 	(9, DEFAULT, 0.1),
 	(10, DEFAULT, 0.3)
+	;
+
+INSERT INTO disc_by VALUES
+	(1, 'Michael', 'Farrell', 20/06/1998),
+	(2, 'Katherine', 'Pearson', 29/09/1991),
+	(2, 'David', 'King', 29/09/1991),
+	(2, 'Amelie', 'Fleming', 29/09/1991),
+	(3, 'Kate', 'Slater', 04/04/1999),
+	(3, 'Freddie', 'Gould', 04/04/1999),
+	(3, 'Sophie', 'Freeman', 04/04/1999)
+	;
+
+INSERT INTO discoverer VALUES
+	('Mr', 'Michael', 'Farrell', 'ESA'),
+	('Prof', 'Katherine', 'Pearson', 'NASA'),
+	('Prof', 'David', 'King', 'NASA'),
+	('Mrs', 'Amelie', 'Fleming', 'NASA'),
+	('Mrs', 'Kate', 'Slater', 'NASA'),
+	('Sir', 'Freddie', 'Gould',),
+	('Prof', 'Sophie', 'Freeman', 'NASA')
 	;
