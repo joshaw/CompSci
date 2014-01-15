58710de6823e1d93bb77d4190875eb4177fd5edb
Josh Wainwright <wainwright.ja@gmail.com>
2014-01-15 16:08:48 +0000

    Added many more functions from the question sheet

--- SoftwareWorkshop2/Worksheet1/Worksheet1.java
+++ SoftwareWorkshop2/Worksheet1/Worksheet1.java
@@ -7,8 +7,20 @@
  * File name : worksheet1.java
  * @version 2014-01-14
  */
+import list.*;
+import static list.List.empty;
+import static list.List.cons;
+import tree.*;
+import static tree.SearchTreeOps.find;
+
 public class Worksheet1 {
 
+	/** Returns true iff all elements of an integer list a greater than zero.
+	 * If the list is empty, then there are no non-positive elements, so true.
+	 *
+	 * @param a list to be checked
+	 * @return true iff all elements are positive, else false.
+	 */
 	public static boolean allPositive(List a) {
 		if (a.isEmpty()) { return true; }
 
@@ -17,9 +29,15 @@ public class Worksheet1 {
 		} else {
 			return allPositive(a.getTail());
 		}
-
 	}
 
+	/** Returns true iff an integer list is sorted in ascending numerical
+	 * order. If the list is empty, or only has a single value, then it must be
+	 * sorted.
+	 *
+	 * @param a list to be checked
+	 * @return true iff the list is sorted, else false.
+	 */
 	public static boolean sorted(List a) {
 		if (a.isEmpty() || a.getTail().isEmpty()) {
 			return true;
@@ -35,6 +53,15 @@ public class Worksheet1 {
 		}
 	}
 
+	/** Merges two sorted integer lists maitaining sorted properties. If either
+	 * list is empty, the other is returned since the merger of a list with an
+	 * empty list is itself.
+	 *
+	 * @param a first integer list to be merged with
+	 * @param b second integer list.
+	 * @return new sorted list with all components of inputs, maintaining any
+	 * duplicates.
+	 */
 	public static List merge(List a, List b) {
 		if (a.isEmpty()) {
 			return b;
@@ -43,22 +70,221 @@ public class Worksheet1 {
 			return a;
 		}
 
-		return merge(a, b, new List());
+		return list.ListOps.reverse(merge(a, b, new List()));
 	}
 
-	public static List merge(List a, List b, List merged) {
-		if (a.getHead().isEmpty()) {
-			
+	/** Auxillary method for merging. Merges two sorted inter lists.
+	 *
+	 * @param a first integer list to be merged with
+	 * @param b second integer list.
+	 * @param merged list of integers holding the intermediate steps of the
+	 * merge as it is performed.
+	 * @return new sorted list with all components of inputs, maintaining any
+	 * duplicates.
+	 */
+	private static List merge(List a, List b, List merged) {
+		if (a.isEmpty()) {
+
+			// add all rest of b to merged
+			merged = preppend(merged, b);
+			return merged;
 		}
+		if (b.isEmpty()) {
+
+			// add all rest of a to merged
+			merged = preppend(merged, a);
+			return merged;
+		}
+
 		int aHead = a.getHead();
 		int bHead = b.getHead();
 
+		/* If the element of the first list is smaller than the second, add it
+		 * to the new list and continue with it's tail and the whole of the
+		 * other list, and vice versa. */
 		if (aHead < bHead) {
-			merge(a.getTail(), b, new List(aHead, merged));
+			return merge(a.getTail(), b, new List(aHead, merged));
 		} else {
-			merge(a, b.getTail(), new List(bHead, merged));
+			return merge(a, b.getTail(), new List(bHead, merged));
+		}
+	}
+
+	/** Removes any duplicate elements from a sorted integer list. If the list
+	 * is empty, then there are no duplicates, so the array is not changed.
+	 *
+	 * @param a list to have duplicates removed from.
+	 * @return new list with no duplicate elements.
+	 */
+	public static List removeDuplicates(List a) {
+		if (a.isEmpty()) {
+			return a;
+		}
+		return list.ListOps.reverse(removeDuplicates(a, new List()));
+	}
+
+	/** Auxillary removeDuplicates method. Removes any duplicates from a sorted
+	 * integer list.
+	 *
+	 * @param a list to have duplicates removed from.
+	 * @param singles list holding the intermediate steps in the duplicate
+	 * removal.
+	 * @return new list with no duplicate elements.
+	 */
+	private static List removeDuplicates(List a, List singles) {
+
+		/* If the tail is empty, there are no more elements to check, so return
+		 * the list with this element added. */
+		if (a.getTail().isEmpty()) {
+			return new List(a.getHead(), singles);
 		}
 
-		return merged;
+		int first = a.getHead();
+		int second = a.getTail().getHead();
+
+		/* If the two elements from the list are different, then add the first
+		 * to the new list, the second will be checked against the next.  */
+		if (first != second) {
+			singles = new List(first, singles);
+		}
+
+		return removeDuplicates(a.getTail(), singles);
+	}
+
+	/** Preppends a list onto the start of another.
+	 *
+	 * @param a original base list
+	 * @param b list to be added to the start of a
+	 * @return new list containing the contents of b then the contents of a.
+	 */
+	public static List preppend(List a, List b) {
+		if (b.isEmpty()) {
+			return a;
+		}
+		if (a.isEmpty()) {
+			return b;
+		}
+		return new List(b.getHead(), preppend(a, b.getTail()));
+	}
+
+	// /** Reverses the order of the elements of an integer list.
+	//  *
+	//  * @param a list to be reversed
+	//  * @return new list which is the reverse of the original.
+	//  */
+	// public static List reverse(List a) {
+	// 	return reverse(a, new List());
+	// }
+
+	// /** Auxillary reverse method. Reverses the order of the elements of a list.
+	//  *
+	//  * @param a list to be reversed
+	//  * @param reversed list holding the intermediate steps in the reversal of
+	//  * the original.
+	//  * @return new list which is the reverse of the original.
+	//  */
+	// private static List reverse(List a, List reversed) {
+	// 	if (a.isEmpty()) {
+	// 		return reversed;
+	// 	}
+	// 	reversed = new List(a.getHead(), reversed);
+	// 	return reverse(a.getTail(), reversed);
+	// }
+
+	/** Returns a new tree which is the mirror image of the input tree in the
+	 * vertical plane. If the tree is empty, then it is the mirror image of
+	 * itself.
+	 *
+	 * @param a the tree to be reflected
+	 * @return a mirror of a.
+	 */
+	public static Tree mirror(Tree a) {
+		if (a.getEmpty()) {
+			return a;
+		}
+		return new Tree(a.getValue(),
+				mirror(a.getRight()), mirror(a.getLeft()));
+	}
+
+	/** Print the elemens of a sorted binary tree in descending order
+	 *
+	 * @param a a tree of which to print the elements of.
+	 */
+	public static void printDescending(Tree a) {
+		if (!a.getEmpty()) {
+			printDescending(a.getRight());
+			System.out.print(a.getValue() + " ");
+			printDescending(a.getLeft());
+		}
+	}
+
+	/** Returns the maximum element from an unsorted binary tree.
+	 *
+	 * @param a tree to return the max element from.
+	 * @return the maximum element.
+	 */
+	public static int maxUnsorted(Tree a) {
+		if (a.getEmpty()) {
+			throw new IllegalStateException("tree has no elements.");
+		} else if (a.getLeft().getEmpty() && a.getRight().getEmpty()) {
+			return a.getValue();
+		} else if (a.getLeft().getEmpty()) {
+			return Math.max(a.getValue(), max(a.getRight()));
+		} else if (a.getRight().getEmpty()) {
+			return Math.max(a.getValue(), max(a.getLeft()));
+		} else {
+			return Math.max(max(a.getLeft()), max(a.getRight()));
+		}
+	}
+
+	/** Returns the maximum element from a sorted binary tree.
+	 *
+	 * @param a tree to return the max element from.
+	 * @return the maximum element.
+	 */
+	public static int max(Tree a) {
+		if (a.getEmpty()) {
+
+			// There are no elements to find the max of.
+			throw new IllegalStateException("tree has no elements.");
+
+		} else if (a.getRight().getEmpty()) {
+
+			// If there is nothing to the right, this must be the largest.
+			return a.getValue();
+
+		} else {
+
+			// Otherwise find the largest elements on the right side.
+			return max(a.getRight());
+		}
+	}
+
+	/** Returns a tree which is the same as the input, but with a single value
+	 * removed.
+	 *
+	 * @param a the tree to remove a value from
+	 * @param x the value to remove from the tree
+	 * @return a new tree without one copy of the value x.
+	 */
+	public static Tree delete(Tree a, int x) {
+		Tree subtree = tree.SearchTreeOps.find(x, a);
+		return delete(a, x, subtree, new Tree());
+	}
+
+	public static Tree delete(Tree a, int x, Tree subtree, Tree newTree) {
+		if (subtree.getLeft().getEmpty() && subtree.getRight().getEmpty()) {
+			if (a == subtree) {
+				// Just delete the node since it is a leaf
+			} else {
+				// add to newtree
+			}
+		} else if (subtree.getLeft().getEmpty()) {
+			// Make child node the new root
+		} else if (subtree.getRight().getEmpty()) {
+			// Make child node the new root
+		} else {
+			// Largest child from left subtree becomes new root.
+		}
+		return new Tree();
 	}
 }
--- SoftwareWorkshop2/Worksheet1/Worksheet1Test.java
+++ SoftwareWorkshop2/Worksheet1/Worksheet1Test.java
@@ -1,22 +1,63 @@
+import list.*;
+import tree.*;
+
 public class Worksheet1Test {
 
 	public static void main(String[] args) {
 
-		List a = new List(2, new List(3, new List(4, new List())));
-		List b = new List(2, new List(-3, new List(4, new List())));
+		List la = new List(2, new List(3, new List(4, new List())));
+		List lb = new List(2, new List(-3, new List(4, new List())));
+		List lc = new List();
+		List ld = new List(2, new List(5, new List(5, new List(8, new List()))));
+		List le = new List(5, new List(7, new List(8, new List(9, new List()))));
+		List lf = new List(2, new List(5, new List(5, new List(5, new List(7,
+						new List(8, new List(8, new List(9, new List()))))))));
 
 		System.out.println("allPositive");
-		System.out.println(Worksheet1.allPositive(a));
-		System.out.println(Worksheet1.allPositive(b));
+		System.out.println(la + " " + Worksheet1.allPositive(la));
+		System.out.println(lb + " " + Worksheet1.allPositive(lb));
 
 		System.out.println("\nsorted");
-		System.out.println(Worksheet1.sorted(a));
-		System.out.println(Worksheet1.sorted(b));
+		System.out.println(la + " " + Worksheet1.sorted(la));
+		System.out.println(lb + " " + Worksheet1.sorted(lb));
 
 		System.out.println("\nmerge");
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(Worksheet1.merge(a, b));
+		System.out.println(ld);
+		System.out.println(le);
+		System.out.println(Worksheet1.merge(ld, le));
+
+		// System.out.println("\npreppend");
+		// System.out.println(d);
+		// System.out.println(e);
+		// System.out.println(Worksheet1.preppend(d, e));
+
+		// System.out.println("\nreverse");
+		// System.out.println(la);
+		// System.out.println(Worksheet1.reverse(la));
+
+		System.out.println("\nremoveDuplicates");
+		System.out.println(lf);
+		System.out.println(Worksheet1.removeDuplicates(lf));
+
+		Tree ta = new Tree(5,
+				new Tree(3,
+					new Tree(1, Tree.emptyTree, Tree.emptyTree),
+					new Tree(4, Tree.emptyTree, Tree.emptyTree)
+				),
+				new Tree(8,
+					new Tree(6, Tree.emptyTree, Tree.emptyTree),
+					Tree.emptyTree
+				)
+			);
+
+		System.out.println(ta);
+		System.out.println(Worksheet1.mirror(ta));
+
+		System.out.println("\nprintDescending");
+		Worksheet1.printDescending(ta);
+
+		System.out.println("\nmax");
+		System.out.println(Worksheet1.max(ta));
 	}
 
 }
