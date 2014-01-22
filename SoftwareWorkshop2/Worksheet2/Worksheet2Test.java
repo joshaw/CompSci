08a69d0c15da672d2592f72bc6fcf730a3e749fc
Josh Wainwright <wainwright.ja@gmail.com>
2014-01-22 11:20:43 +0000

    Move all generation of trees to BeforeClass class

--- SoftwareWorkshop2/Worksheet2/Worksheet2Test.java
+++ SoftwareWorkshop2/Worksheet2/Worksheet2Test.java
@@ -11,6 +11,7 @@ import tree.*;
 
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import static org.junit.Assert.*;
 
 public class Worksheet2Test {
@@ -33,20 +34,21 @@ public class Worksheet2Test {
 	static Tree tUnbalancedSearch;
 	static Tree tUnbalancedNonSearch;
 	static Tree ta;
+	static Tree taAddHBComp;
+	static Tree tEmptyAddHBComp;
 
-	// Create tree for testing
-	@Before
-	public void makeTree() {
+	@BeforeClass
+	public static void makeTreeNonSearch() {
+
+		// --------------------------------------------------------------------
 		int[] numbers = {1, 5, 9, 2, 8, 4, 7, 3, 6};
 		tUnbalancedSearch = new Tree();
 		for (int i = 0; i < numbers.length; i++) {
 			tUnbalancedSearch = tree.SearchTreeOps.insert(numbers[i],
 					tUnbalancedSearch);
 		}
-	}
 
-	@Before
-	public void makeTreeNonSearch() {
+		// --------------------------------------------------------------------
 		tUnbalancedNonSearch = new Tree(3,
 								new Tree(4,
 									new Tree(),
@@ -57,19 +59,26 @@ public class Worksheet2Test {
 									new Tree()
 								)
 							   );
-	}
 
-	@Before
-	public void makeBalancedTree() {
-		int[] numbers = {6, 3, 8, 1, 5, 7, 9, 2, 4};
+		// --------------------------------------------------------------------
+		int[] numbers2 = {6, 3, 8, 1, 5, 7, 9, 2, 4};
 		ta = new Tree();
-		for (int i = 0; i < numbers.length; i++) {
-			ta = tree.SearchTreeOps.insert(numbers[i], ta);
+		for (int i = 0; i < numbers2.length; i++) {
+			ta = tree.SearchTreeOps.insert(numbers2[i], ta);
 		}
+
+		// --------------------------------------------------------------------
+		int[] numbers3 = {6, 3, 8, 1, 5, 7, 9, 2, 4, 10};
+		taAddHBComp = new Tree();
+		for (int i = 0; i < numbers3.length; i++) {
+			taAddHBComp = tree.SearchTreeOps.insert(numbers3[i], taAddHBComp);
+		}
+
+		// --------------------------------------------------------------------
+		tEmptyAddHBComp = new Tree(1, new Tree(), new Tree());
 	}
 
-	/* Some comme
-	 * */
+	// ------------------------------------------------------------------------
 	@Test
 	public void testGetHeight() {
 		assertEquals(-1, tEmpty.getHeight());
@@ -85,6 +94,7 @@ public class Worksheet2Test {
 		assertEquals(3, ta.getHeight());
 	}
 
+	// ------------------------------------------------------------------------
 	@Test
 	public void testIsHeightBalanced() {
 		assertTrue(Worksheet2.isHeightBalanced(ta));
@@ -95,6 +105,7 @@ public class Worksheet2Test {
 		assertFalse(Worksheet2.isHeightBalanced(tUnbalancedSearch));
 	}
 
+	// ------------------------------------------------------------------------
 	@Test
 	public void testIsSearchTree() {
 		assertTrue(Worksheet2.isSearchTree(ta));
