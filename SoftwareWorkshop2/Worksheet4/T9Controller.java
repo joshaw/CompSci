69b0562660e3e390cc746d4b62bbc49af08e1f6e
Josh Wainwright (uni) <jaw097@bham.ac.uk>
2014-02-17 12:44:44 +0000

    add button recognition

--- SoftwareWorkshop2/Worksheet4/T9Controller.java
+++ SoftwareWorkshop2/Worksheet4/T9Controller.java
@@ -1,23 +1,51 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class T9Controller {
 
-    private T9Model model;
-    private T9View view;
+	private T9Model model;
+	private T9View view;
 
-    public T9Controller(T9Model model, T9View view) {
-        this.model = model;
-        this.view = view;
+	private ArrayList<String> currentWords;
+	private int sel = 0;
+	private String curWord = "";
 
-        view.addButtonListner(new ButtonListener());
-    }
+	public T9Controller(T9Model model, T9View view) {
+		this.model = model;
+		this.view = view;
 
-    class ButtonListener implements ActionListener {
-    	public void actionPerformed(ActionEvent e) {
-    		System.out.println(e.getActionCommand());
-    		view.setTextArea(e.getActionCommand());
-    	}
-    }
+		view.addButtonListner(new ButtonListener());
+	}
+
+	private String getWord(int i) {
+		String temp = currentWords.get(i);
+		return "THIS";
+	}
+
+	class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+
+			int buttonNumber = Integer.parseInt(e.getActionCommand());
+
+			String oldText = view.getTextArea();
+
+			if (buttonNumber > 0 && buttonNumber <= 9) {
+
+				currentWords = model.wordPrefixList("" + buttonNumber);
+				view.setTextArea(getWord(sel));
+
+			} else if (buttonNumber == 10) {
+				sel++;
+				view.setTextArea(getWord(sel));
+			} else if (buttonNumber == 11) {
+				curWord = "";
+				sel = 0;
+				view.reset();
+			} else if (buttonNumber == 12) {
+				view.setTextArea("Hash button pressed");
+			}
+		}
+	}
 
 }
--- SoftwareWorkshop2/Worksheet4/T9Model.java
+++ SoftwareWorkshop2/Worksheet4/T9Model.java
@@ -3,24 +3,37 @@
 #############################################################################*/
 
 import java.util.Observable;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+import predictive.*;
+
 public final class T9Model {
 
-	private static final String INITIAL_VALUE = "0";
 	private int total;
+	private TreeDictionary dict;
 
 	T9Model() {
-		reset();
+		dict = new TreeDictionary("words.txt");
 	}
 
-	public void reset() {
-		total = 0;
+	public ArrayList<String> wordPrefixList(String sig) {
+		Set<String> wordSet = dict.signatureToWords(sig);
+		return setToArrayList(wordSet);
+	}
+
+	private ArrayList<String> setToArrayList(Set<String> set) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.addAll(set);
+		return list;
 	}
 
 	public void setValue(String val) {
 		System.out.println("Value = " + val);
 	}
 
-    public void printValue(int val) {
-        System.out.println("Int = " + val);
-    }
+	public void printValue(int val) {
+		System.out.println("Int = " + val);
+	}
 }
